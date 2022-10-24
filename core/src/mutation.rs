ea64d442966605174633b0c2cc4501ac86124bf3
Sebastian Pravda <sebastian.pravda@gmail.com>
2022-10-24 14:43:26 +0200

    remove all post entity imports

--- core/src/mutation.rs
+++ core/src/mutation.rs
@@ -1,53 +1,13 @@
-use ::entity::{post, post::Entity as Post};
+use ::entity::{candidate, candidate::Entity as Candidate};
 use sea_orm::*;
 
 pub struct Mutation;
 
 impl Mutation {
-    pub async fn create_post(
+    pub async fn create_candidate(
         db: &DbConn,
-        form_data: post::Model,
-    ) -> Result<post::ActiveModel, DbErr> {
-        post::ActiveModel {
-            title: Set(form_data.title.to_owned()),
-            text: Set(form_data.text.to_owned()),
-            ..Default::default()
-        }
-        .save(db)
-        .await
-    }
-
-    pub async fn update_post_by_id(
-        db: &DbConn,
-        id: i32,
-        form_data: post::Model,
-    ) -> Result<post::Model, DbErr> {
-        let post: post::ActiveModel = Post::find_by_id(id)
-            .one(db)
-            .await?
-            .ok_or(DbErr::Custom("Cannot find post.".to_owned()))
-            .map(Into::into)?;
-
-        post::ActiveModel {
-            id: post.id,
-            title: Set(form_data.title.to_owned()),
-            text: Set(form_data.text.to_owned()),
-        }
-        .update(db)
-        .await
-    }
-
-    pub async fn delete_post(db: &DbConn, id: i32) -> Result<DeleteResult, DbErr> {
-        let post: post::ActiveModel = Post::find_by_id(id)
-            .one(db)
-            .await?
-            .ok_or(DbErr::Custom("Cannot find post.".to_owned()))
-            .map(Into::into)?;
-
-        post.delete(db).await
-    }
-
-    pub async fn delete_all_posts(db: &DbConn) -> Result<DeleteResult, DbErr> {
-        Post::delete_many().exec(db).await
+        form_data: candidate::Model,
+    ) -> Result<candidate::ActiveModel, DbErr> {
+        todo!()
     }
 }
--- core/src/query.rs
+++ core/src/query.rs
@@ -1,26 +1,10 @@
-use ::entity::{post, post::Entity as Post};
+use ::entity::{candidate, candidate::Entity as Candidate};
 use sea_orm::*;
 
 pub struct Query;
 
 impl Query {
-    pub async fn find_post_by_id(db: &DbConn, id: i32) -> Result<Option<post::Model>, DbErr> {
-        Post::find_by_id(id).one(db).await
-    }
-
-    /// If ok, returns (post models, num pages).
-    pub async fn find_posts_in_page(
-        db: &DbConn,
-        page: u64,
-        posts_per_page: u64,
-    ) -> Result<(Vec<post::Model>, u64), DbErr> {
-        // Setup paginator
-        let paginator = Post::find()
-            .order_by_asc(post::Column::Id)
-            .paginate(db, posts_per_page);
-        let num_pages = paginator.num_pages().await?;
-
-        // Fetch paginated posts
-        paginator.fetch_page(page - 1).await.map(|p| (p, num_pages))
+    pub async fn find_candidate_by_id(db: &DbConn, id: i32) -> Result<Option<candidate::Model>, DbErr> {
+        Candidate::find_by_id(id).one(db).await
     }
 }
