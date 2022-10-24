3fb5a51cfb1214a7a31e6ce4f65108c14a625551
EETagent <vojta.jungmann@gmail.com>
2022-10-24 12:53:58 +0200

    chore: parent migrace

--- entity/src/prelude.rs
+++ entity/src/prelude.rs
@@ -1,2 +1,3 @@
 pub use super::admin::Entity as Admin;
 pub use super::candidate::Entity as Candidate;
+pub use super::parent::Entity as Parent;
--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -2,12 +2,17 @@ pub use sea_orm_migration::prelude::*;
 
 mod m20221024_111310_create_admin;
 mod m20221024_121621_create_candidate;
+mod m20221024_124701_create_parent;
 
 pub struct Migrator;
 
 #[async_trait::async_trait]
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
-        vec![Box::new(m20221024_111310_create_admin::Migration), Box::new(m20221024_121621_create_candidate::Migration)]
+        vec![
+            Box::new(m20221024_111310_create_admin::Migration),
+            Box::new(m20221024_121621_create_candidate::Migration),
+            Box::new(m20221024_124701_create_parent::Migration),
+        ]
     }
 }
