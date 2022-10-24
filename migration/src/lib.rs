782d14384f72dff9174c91d5f27d08c6b30ecde7
EETagent <vojta.jungmann@gmail.com>
2022-10-24 12:35:56 +0200

    chore: import candidate migrace

--- migration/src/lib.rs
+++ migration/src/lib.rs
@@ -1,12 +1,13 @@
 pub use sea_orm_migration::prelude::*;
 
 mod m20221024_111310_create_admin;
+mod m20221024_121621_create_candidate;
 
 pub struct Migrator;
 
 #[async_trait::async_trait]
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
-        vec![Box::new(m20221024_111310_create_admin::Migration)]
+        vec![Box::new(m20221024_111310_create_admin::Migration), Box::new(m20221024_121621_create_candidate::Migration)]
     }
 }
