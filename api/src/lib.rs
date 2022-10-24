c47dfbb1f900b7183f116c191ce7e4557a74ea19
Sebastian Pravda <34771614+starvy@users.noreply.github.com>
2022-10-24 15:00:49 +0200

    Refactoring aby to šlo spustit (#4)
    * db: candidate entity
    
    * chore: candidate migrace
    
    * chore: import candidate migrace
    
    * fix: default value pro citizenship
    
    * fix: unique pro evideční číslo přihlášky
    
    * chore: parent migrace
    
    * fix: unique pro evideční číslo přihlášky
    
    * chore: sample fill admin table
    
    * fix: chybějící serde pro admin entitu
    
    * chore: remove mock tests
    
    * remove all post entity imports
    
    Co-authored-by: EETagent <vojta.jungmann@gmail.com>

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -1,5 +1,6 @@
 #[macro_use]
 extern crate rocket;
+use rocket::serde::json::Json;
 
 use rocket::fairing::{self, AdHoc};
 use rocket::form::{ Form};
@@ -14,60 +15,26 @@ use sea_orm_rocket::{Connection, Database};
 mod pool;
 use pool::Db;
 
-pub use entity::post;
-pub use entity::post::Entity as Post;
+pub use entity::candidate;
+pub use entity::candidate::Entity as Candidate;
 
 
 #[post("/", data = "<post_form>")]
-async fn create(conn: Connection<'_, Db>, post_form: Form<post::Model>) -> Flash<Redirect> {
+async fn create(conn: Connection<'_, Db>, post_form: Json<candidate::Model>) -> Flash<Redirect> {
     let db = conn.into_inner();
 
     let form = post_form.into_inner();
 
-    Mutation::create_post(db, form)
+    Mutation::create_candidate(db, form)
         .await
         .expect("could not insert post");
 
     Flash::success(Redirect::to("/"), "Post successfully added.")
 }
 
-#[post("/<id>", data = "<post_form>")]
-async fn update(
-    conn: Connection<'_, Db>,
-    id: i32,
-    post_form: Form<post::Model>,
-) -> Flash<Redirect> {
-    let db = conn.into_inner();
-
-    let form = post_form.into_inner();
-
-    Mutation::update_post_by_id(db, id, form)
-        .await
-        .expect("could not update post");
-
-    Flash::success(Redirect::to("/"), "Post successfully edited.")
-}
-
-#[delete("/<id>")]
-async fn delete(conn: Connection<'_, Db>, id: i32) -> Flash<Redirect> {
-    let db = conn.into_inner();
-
-    Mutation::delete_post(db, id)
-        .await
-        .expect("could not delete post");
-
-    Flash::success(Redirect::to("/"), "Post successfully deleted.")
-}
-
-#[delete("/")]
-async fn destroy(conn: Connection<'_, Db>) -> Result<(), rocket::response::Debug<String>> {
-    let db = conn.into_inner();
-
-    Mutation::delete_all_posts(db)
-        .await
-        .map_err(|e| e.to_string())?;
-
-    Ok(())
+#[get("/hello")]
+async fn hello() -> &'static str {
+    "Hello, world!"
 }
 
 async fn run_migrations(rocket: Rocket<Build>) -> fairing::Result {
@@ -82,7 +49,7 @@ async fn start() -> Result<(), rocket::Error> {
         .attach(Db::init())
         .attach(AdHoc::try_on_ignite("Migrations", run_migrations))
         //.mount("/", FileServer::from(relative!("/static")))
-        .mount("/", routes![create, delete, destroy, update])
+        .mount("/", routes![create, hello])
         .register("/", catchers![])
         .launch()
         .await
