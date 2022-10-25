7e9f63afe284d0fa32e19505840135e695157371
Sebastian Pravda <sebastian.pravda@gmail.com>
2022-10-25 17:00:30 +0200

    chore: session migrace

--- entity/src/lib.rs
+++ entity/src/lib.rs
@@ -3,3 +3,4 @@ pub mod prelude;
 pub mod admin;
 pub mod candidate;
 pub mod parent;
+pub mod session;
--- entity/src/prelude.rs
+++ entity/src/prelude.rs
@@ -3,3 +3,4 @@
 pub use super::admin::Entity as Admin;
 pub use super::candidate::Entity as Candidate;
 pub use super::parent::Entity as Parent;
+pub use super::session::Entity as Session;
