bd72aa1a6f3801bcb0b390d14bb49b03443e6a30
EETagent <vojta.jungmann@gmail.com>
2022-10-28 13:42:44 +0200

    feat: tests for crypto functions

--- core/src/crypto.rs
+++ core/src/crypto.rs
@@ -165,3 +165,123 @@ pub async fn decrypt_password_with_private_key(
 
     Ok(String::from_utf8(decrypt_buffer)?)
 }
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn test_random_8_char_string() {
+        let s = super::random_8_char_string();
+        // Is 8 chars long
+        assert_eq!(s.len(), 8);
+        // Does not contain possibly confusing characters
+        assert!(!s.contains('0'));
+        assert!(!s.contains('O'));
+    }
+
+    #[tokio::test]
+    async fn test_hash_password() {
+        const PASSWORD: &str = "test";
+        let hash = super::hash_password(PASSWORD.to_string()).await.unwrap();
+
+        assert!(hash.contains("$argon2"));
+    }
+
+    #[tokio::test]
+    async fn test_verify_password() {
+        const HASH: &str = "$argon2id$v=19$m=4096,t=3,p=1$c2VjcmV0bHl0ZXN0aW5nZXZlcnl0aGluZw$xEzH8wD/ZjzgZTDTl3YtzMFCfcVa5M5m9y6NfSyB1n4";
+        const PASSWORD: &str = "test";
+
+        let result = super::verify_password(PASSWORD.to_string(), HASH.to_string()).await.unwrap();
+
+        assert!(result);
+    }
+
+    #[tokio::test]
+    async fn test_hash_and_verify_password() {
+        const PASSWORD: &str = "test";
+
+        let hash = super::hash_password(PASSWORD.to_string()).await.unwrap();
+
+        let result = super::verify_password(PASSWORD.to_string(), hash).await.unwrap();
+
+        assert!(result);
+    }
+    
+    #[tokio::test]
+    async fn test_encrypt_password_is_valid_base64() {
+        const PASSWORD: &str = "test";
+        const KEY: &str = "test";
+
+        let encrypted = super::encrypt_password(PASSWORD, KEY).await.unwrap();
+
+        assert!(base64::decode(encrypted).is_ok());
+    }
+
+    #[tokio::test]
+    async fn test_encrypt_decrypt_password() {
+        const PASSWORD: &str = "test";
+        const KEY: &str = "test";
+
+        let encrypted = super::encrypt_password(PASSWORD, KEY).await.unwrap();
+
+        let decrypted = super::decrypt_password(&encrypted, KEY).await.unwrap();
+
+        assert_eq!(PASSWORD, decrypted);
+    }
+
+    #[tokio::test]
+    async fn test_encrypt_password_with_recipients_is_valid_base64() {
+        const PASSWORD: &str = "test";
+        const PUBLIC_KEY: &str = "age1t220v5c8ye0pjx99kw8nr57y7a5qlw4ke0wchjuxnr2gcvfzt3hq7fufz0";
+
+        let encrypted = super::encrypt_password_with_recipients(PASSWORD, vec![PUBLIC_KEY]).await.unwrap();
+
+        eprint!("{}", encrypted);
+
+        assert!(base64::decode(encrypted).is_ok());
+    }
+
+    #[tokio::test]
+    async fn test_encrypt_password_with_recipients_multiple_is_valid_base64() {
+        const PASSWORD: &str = "test";
+        const PUBLIC_KEY_1: &str = "age1t220v5c8ye0pjx99kw8nr57y7a5qlw4ke0wchjuxnr2gcvfzt3hq7fufz0";
+        const PUBLIC_KEY_2: &str = "age1ygswsk38cq9r64um5klqxyvzemfdvx6qe5zed99pdexakwwhpatsgatgpw";
+
+        let encrypted = super::encrypt_password_with_recipients(PASSWORD, vec![PUBLIC_KEY_1, PUBLIC_KEY_2]).await.unwrap();
+
+        println!("{}", encrypted);
+        assert!(base64::decode(encrypted).is_ok());
+    }
+
+    #[tokio::test]
+    async fn test_decrypt_password_with_private_key() {
+        const PASSWORD: &str = "test";
+        //const PUBLIC_KEY: &str = "age1t220v5c8ye0pjx99kw8nr57y7a5qlw4ke0wchjuxnr2gcvfzt3hq7fufz0";
+        const PRIVATE_KEY: &str = "AGE-SECRET-KEY-1WPDHL2FLJ23T6RK5KCX8KS8DNLX0CGXMNZG0XNUAH4QP5C8ZZ46QGD3STV";
+        const CIPHER: &str = "YWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBVWUNCY0RielVCaThLbGlIR1NZa0p6MlNiS0x5L3B2Y3B2b21XZHNaZUVjClpsVTRvUGVVQVYzS205VTVVMDlXYjFHVE5ZZzJOSEpyN1ZyT0tocFpIbUUKLT4gPy1ncmVhc2UgLltXKT9MJyBLQGouLWcgfCBQSm12JQp3bDhRTDd0ZGZWbU9mQ2FYVU9Cb2FjM3AwR243OGJNCi0tLSBSSzRxV3E2d0VscERvM3VHVUhOL3dPaGVBRHE3WkZrdzYxYUgyQVl6elh3CiFQOr28YvbEAkx0YgFnIxwvPNjjYZV6THArcMPM8i5flnmKPw==";
+
+        let decrypted = super::decrypt_password_with_private_key(CIPHER, PRIVATE_KEY).await.unwrap();
+
+        assert_eq!(PASSWORD, decrypted);
+    }
+
+    #[tokio::test]
+    async fn test_decrypt_password_with_private_key_multiple() {
+        const PASSWORD: &str = "test";
+        // const PUBLIC_KEY_1: &str = "age1t220v5c8ye0pjx99kw8nr57y7a5qlw4ke0wchjuxnr2gcvfzt3hq7fufz0";
+        // const PUBLIC_KEY_2: &str = "age1ygswsk38cq9r64um5klqxyvzemfdvx6qe5zed99pdexakwwhpatsgatgpw";
+        const PRIVATE_KEY_1: &str = "AGE-SECRET-KEY-1WPDHL2FLJ23T6RK5KCX8KS8DNLX0CGXMNZG0XNUAH4QP5C8ZZ46QGD3STV";
+        const PRIVATE_KEY_2: &str = "AGE-SECRET-KEY-19RT6Z6TR0TE465EMJFDVXAFZ00YE65THLSS5LAY4W85L587DF95SPPDVND";
+
+        const CIPHER: &str = "YWdlLWVuY3J5cHRpb24ub3JnL3YxCi0+IFgyNTUxOSBBQ1BuSi9VMWIzeHg1TjQwMDNSUzlpZ0pGRWMxU2pFenVBekxGQTM0WGkwClkycytsNXNMbmVJTm5GT3VDRFBGQXE1ZFU5MzNzV0NXRWhmV1VGSjVNbU0KLT4gWDI1NTE5IHAvUjRLc3ROd2FkalZWTVIxRnBjaEluMXNtYWVScTVxdWxHY0x6ajZtUmMKYXkyNTExakZ0NWt5Vm85YUJSRlRmZTh4VEEyVEVrOFRyWDMxckNDVGkzOAotPiBbNVhfKS1ncmVhc2UgcysxIChlLTsKYU43T0lXUlUxZDFRVUpacXdJcm02Y3VzSjNMTVBtcy9pNm9yOEdETVplYjJrY1VsemRZU00rZ3NrSFZvUTBoSQovcEVrcmRmYlBPdzN3WWZTR0t1a1VFY0VTWXlIR1VPSUJRCi0tLSBYbmpxUHpVQzl5YnowdktIcTRjTklERXRDYVAxb0FmaWQwazgzRkp0U2pNCiAVlCPJ1+jroWQ7HBqjRUOcCBMyYvi9xIaklX2XDYPB2rd7Fw==";
+
+        let decrypted_1 = super::decrypt_password_with_private_key(CIPHER, PRIVATE_KEY_1).await.unwrap();
+
+        assert_eq!(PASSWORD, decrypted_1);
+
+        let decrypted_2 = super::decrypt_password_with_private_key(CIPHER, PRIVATE_KEY_2).await.unwrap();
+
+        assert_eq!(PASSWORD, decrypted_2);
+    }
+
+}
