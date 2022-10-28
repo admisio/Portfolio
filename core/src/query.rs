69b5a4fabf276e73b21a5c31d401fea01ba77c82
Sebastian Pravda <sebastian.pravda@gmail.com>
2022-10-28 12:11:23 +0200

    feat: find session by uuid

--- core/src/query.rs
+++ core/src/query.rs
@@ -1,10 +1,16 @@
 use ::entity::{candidate, candidate::Entity as Candidate};
+use ::entity::{session, session::Entity as Session};
 use sea_orm::*;
+use sea_orm::prelude::Uuid;
 
 pub struct Query;
 
 impl Query {
     pub async fn find_candidate_by_id(db: &DbConn, id: i32) -> Result<Option<candidate::Model>, DbErr> {
         Candidate::find_by_id(id).one(db).await
+    }
+
+    pub async fn find_session_by_uuid(db: &DbConn, uuid: Uuid) -> Result<Option<session::Model>, DbErr> {
+        Session::find_by_id(uuid).one(db).await
     }
 }
