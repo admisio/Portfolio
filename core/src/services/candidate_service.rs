ed783859b7d6d53ec666e36077cfed161788ade1
Sebastian Pravda <sebastian.pravda@gmail.com>
2022-11-04 17:19:13 +0100

    feat: validate application_id

--- core/src/services/candidate_service.rs
+++ core/src/services/candidate_service.rs
@@ -1,19 +1,36 @@
 use entity::candidate;
 use sea_orm::{DbConn, prelude::Uuid};
 
-use crate::{Mutation, crypto::{hash_password, self}, error::{ServiceError}};
+use crate::{Mutation, crypto::{hash_password, self}, error::{ServiceError}, Query};
 
 use super::session_service::SessionService;
+
+const CODES: [&str; 3] = ["101", "102", "103"];
 
 pub struct CandidateService;
 
 impl CandidateService {
+    /// Creates a new candidate with:
+    /// Encrypted personal identification number
+    /// Hashed password
+    /// Encrypted private key
+    /// Public key
     pub async fn create(
         db: &DbConn,
         application_id: i32,
         plain_text_password: &String,
         personal_id_number: String
     ) -> Result<candidate::Model, ServiceError>{
+        // Check if application id starts with 101, 102 or 103
+        if !CandidateService::is_application_id_valid(application_id) {
+            return Err(ServiceError::InvalidApplicationId)
+        }
+
+        // Check if user with that application id already exists
+        if Query::find_candidate_by_id(db, application_id).await.unwrap().is_some() {
+            return Err(ServiceError::UserAlreadyExists)
+        }
+
         // TODO: unwrap pro testing..
         let hashed_password = hash_password(plain_text_password.to_string()).await.unwrap();
         let (pubkey, priv_key_plain_text) = crypto::create_identity();
@@ -50,6 +67,15 @@ impl CandidateService {
     ) -> Result<candidate::Model, ServiceError> {
         SessionService::auth_user_session(db, session_uuid).await
     }
+
+    fn is_application_id_valid(application_id: i32) -> bool {
+        let s = &application_id.to_string();
+        if s.len() <= 3 { // TODO: does the code have to be exactly 6 digits?
+            return false;
+        }
+        let code = &s[0..3];
+        CODES.contains(&code)
+    }
 }
 
 
@@ -58,6 +84,17 @@ mod tests {
     use sea_orm::{Database, DbConn};
 
     use crate::{crypto, services::candidate_service::CandidateService};
+
+    #[tokio::test]
+    async fn test_application_id_validation() {
+        assert!(CandidateService::is_application_id_valid(101_101));
+        assert!(CandidateService::is_application_id_valid(102_107));
+        assert!(CandidateService::is_application_id_valid(103_109));
+        assert!(!CandidateService::is_application_id_valid(104_109));
+        assert!(!CandidateService::is_application_id_valid(100_109));
+        assert!(!CandidateService::is_application_id_valid(201_109));
+        assert!(!CandidateService::is_application_id_valid(101));
+    }
 
     #[cfg(test)]
     async fn get_memory_sqlite_connection() -> DbConn {
