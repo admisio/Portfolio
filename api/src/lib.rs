27a7845b7f5ad267a9f6af31752ff0ae0dc7531d
EETagent <vojta.jungmann@gmail.com>
2022-11-07 20:26:13 +0100

    feat: add upload endpoints to API

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -42,6 +42,9 @@ async fn start() -> Result<(), rocket::Error> {
                 routes::candidate::login,
                 routes::candidate::whoami,
                 routes::candidate::fill_details,
+                routes::candidate::upload_cover_letter,
+                routes::candidate::upload_portfolio_letter,
+                routes::candidate::upload_portfolio_zip,
             ],
         )
         .mount(
