f6e45dc89b3b4f9d5ec9e4f6d63073d632ed0d7b
EETagent <vojta.jungmann@gmail.com>
2022-11-07 13:28:23 +0100

    feat: add admin login request

--- api/src/requests.rs
+++ api/src/requests.rs
@@ -14,3 +14,11 @@ pub struct RegisterRequest {
     pub application_id: i32,
     pub personal_id_number: String,
 }
+
+
+#[derive(Serialize, Deserialize)]
+#[serde(crate = "rocket::serde")]
+pub struct AdminLoginRequest {
+    pub admin_id: i32,
+    pub password: String,
+}
