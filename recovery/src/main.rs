693426f0411d780c15a6c9e9222533ab5f4b690f
EETagent <vojta.jungmann@gmail.com>
2022-11-11 20:20:48 +0100

    feat: add useful utilities

--- recovery/src/main.rs
+++ recovery/src/main.rs
@@ -1,60 +1,127 @@
 use std::path::PathBuf;
 use url::Url;
 
-use clap::{arg, command, value_parser};
+use clap::{arg, command, value_parser, Command, ArgAction};
 use sea_orm::{Database, DatabaseConnection};
 
-use sea_orm::*;
 use ::entity::candidate::Entity as Candidate;
 use ::entity::parent::Entity as Parent;
+use sea_orm::*;
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let clap = command!()
-        .arg(
-            arg!(
-                -d --database <URL> "URL to the database or sql file with postgres:// or sqlite://"
-            )
-            .alias("url")
-            .required(true)
-            .value_parser(value_parser!(Url)),
+        .propagate_version(true)
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .subcommand(
+            Command::new("portfolio")
+                .about("Database & Portfolio operations")
+                .arg(
+                    arg!(
+                        -d --database <URL> "URL to the database or sql file with postgres:// or sqlite://"
+                    )
+                    .alias("url")
+                    .required(true)
+                    .value_parser(value_parser!(Url)),
+                )
+                .arg(
+                    arg!(
+                        -p --portfolio <PATH>  "Path to the portfolio root"
+                    )
+                    .required(true)
+                    .value_parser(value_parser!(PathBuf)),
+                )
+                .arg(
+                    arg!(
+                        -k --key <KEY> "AGE private key for decryption"
+                    )
+                    .required(true),
+                )
         )
-        .arg(
-            arg!(
-                -p --portfolio <PATH>  "Path to the portfolio root"
-            )
-            .required(true)
-            .value_parser(value_parser!(PathBuf)),
+        .subcommand(
+            Command::new("hash")
+                .about("Hash operations")
+                .arg(
+                    arg!(
+                        -i --input <INPUT> "Plaintext to hash"
+                    )
+                    .required(true)
+                ),
         )
-        .arg(
-            arg!(
-                -k --key <KEY> "AGE private key for decryption"
-            )
-            .required(true),
+        .subcommand(
+            Command::new("encryption")
+                .about("Encryption operations")
+                .arg(
+                    arg!(
+                        -d --decrypt ... "Decrypt flag"
+                    )
+                    .action(ArgAction::SetFalse)
+                    .required(false)
+                )
+                .arg(
+                    arg!(
+                        -i --input <INPUT> "Plaintext to encrypt/decrypt"
+                    )
+                    .required(true)
+                )
+                .arg(
+                    arg!(
+                        -k --key <KEY> "Key for encryption/decryption"
+                    )
+                    .required(true),
+                )
         )
         .get_matches();
 
-    let sqlite_url = clap.get_one::<Url>("database").unwrap();
+    match clap.subcommand() {
+        Some(("portfolio", sub_matches)) => {
+            let sqlite_url = sub_matches.get_one::<Url>("database").unwrap();
 
-    println!("Connecting to {:?}", sqlite_url);
+            println!("Connecting to {:?}", sqlite_url);
 
-    if sqlite_url.scheme() != "sqlite" && sqlite_url.scheme() != "postgres" {
-        return Err("URL scheme postgres:// or sqlite:// required")?;
+            if sqlite_url.scheme() != "sqlite" && sqlite_url.scheme() != "postgres" {
+                return Err("URL scheme postgres:// or sqlite:// required")?;
+            }
+
+            let db: DatabaseConnection = Database::connect(sqlite_url.as_str()).await?;
+
+            let entries = Candidate::find()
+                .join_rev(
+                    JoinType::InnerJoin,
+                    Parent::belongs_to(Candidate)
+                        .from(::entity::parent::Column::Application)
+                        .to(::entity::candidate::Column::Application)
+                        .into(),
+                )
+                .all(&db)
+                .await?;
+
+            println!("Found {} entries", entries.len());
+        }
+        Some(("hash", sub_matches)) => {
+            let input = sub_matches.get_one::<String>("input").unwrap();
+
+            let hash = portfolio_core::crypto::hash_password(input.to_string()).await?;
+
+            println!("{}", hash);
+        }
+        Some(("encryption", sub_matches)) => {
+            let decrypt = sub_matches.get_one::<bool>("decrypt").unwrap();
+            let input = sub_matches.get_one::<String>("input").unwrap();
+            let key = sub_matches.get_one::<String>("key").unwrap();
+
+
+            let result = if !*decrypt {
+                portfolio_core::crypto::decrypt_password(input.to_string(), key.to_string()).await?
+            } else {
+                portfolio_core::crypto::encrypt_password(input.to_string(), key.to_string()).await?
+            };
+
+            println!("{}", result);
+        }
+        _ => unreachable!("Exhausted list of subcommands and subcommand_required prevents `None`"),
     }
 
-    let db: DatabaseConnection = Database::connect(sqlite_url.as_str()).await?;
-
-    let entries = Candidate::find()
-        .join_rev(
-            JoinType::InnerJoin,
-            Parent::belongs_to(Candidate)
-                .from(::entity::parent::Column::Application)
-                .to(::entity::candidate::Column::Application)
-                .into(),
-        )
-        .all(&db)
-        .await?;
-
     Ok(())
-    
 }
