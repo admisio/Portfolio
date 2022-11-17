b05685a5df6650882af3332b6f9bb8c841029418
EETagent <vojta.jungmann@gmail.com>
2022-11-17 19:53:14 +0100

    feat: add Poppins font

--- frontend/src/routes/+layout.svelte
+++ frontend/src/routes/+layout.svelte
@@ -1,4 +1,14 @@
+<link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" />
+<link
+	href="https://fonts.googleapis.com/css2?family=Poppins:wght@200;400;600&display=swap"
+	rel="stylesheet"
+/>
+
 <slot />
 
 <style windi:preflights:global windi:safelist:global>
+	:global(html) {
+		font-family: 'Poppins', sans-serif;
+	}
 </style>
