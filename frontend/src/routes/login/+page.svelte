27b5cf8ae28942e9e1e6fd0e53b56c4858dfc787
EETagent <vojta.jungmann@gmail.com>
2022-11-17 22:54:22 +0100

    feat: full layout & refactoring

--- frontend/src/routes/login/+page.svelte
+++ frontend/src/routes/login/+page.svelte
@@ -10,22 +10,17 @@
 		>
 			<img class="object-cover" src={lev} alt="" />
 		</div>
-		<h1 class="mt-6 text-4xl text-[#406280] font-semibold">Přihlášení</h1>
-		<p class="mt-6 font-light text-[#e6e6e6] text-center">
+		<h1 class="mt-8 text-4xl text-sspsBlue font-semibold">Přihlášení</h1>
+		<p class="mt-8 font-light text-sspsGray text-center">
 			Lorem ipsum dolor sit amet, consectetuer adipiscing elit.<br /> Fusce suscipit libero eget elit.
 		</p>
 		<input
-			class="mt-6 w-3/5 shadow-lg p-3 rounded-lg text-xl"
-			type="text"
-			placeholder="Evidenční číslo přihlášky"
-		/>
-		<input
-			class="mt-6 w-3/5 shadow-lg p-3 rounded-lg text-xl border border-[#406280] border-2"
+			class="bg-[#f8fafb] mt-8 w-3/5 shadow-lg p-3 rounded-lg text-xl outline-none border transition-colors duration-300 hover:border-sspsBlue  border-2"
 			type="password"
-			value="............"
+			placeholder="Ev. číslo"
 		/>
 		<input
-			class="mt-6 w-3/5 p-3 rounded-lg font-semibold text-xl bg-[#406280] text-white"
+			class="mt-8 w-3/5 p-3 rounded-lg font-semibold text-xl transition-colors duration-300 bg-sspsBlue hover:bg-sspsBlueDark text-white"
 			type="submit"
 			value="Odeslat"
 		/>
