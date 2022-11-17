886f4a95dd79a4e1b3748d77541de1749a35cf68
EETagent <vojta.jungmann@gmail.com>
2022-11-17 22:53:55 +0100

    feat: params

---
+++ frontend/src/routes/login/[code=application]/+page.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	import FullLayout from '$lib/components/layout/FullLayout.svelte';
+</script>
+
+<FullLayout />
+
+<style>
+	.form {
+		@apply flex flex-col;
+		@apply mx-auto w-[90%] h-full;
+		@apply items-center justify-center;
+	}
+</style>
