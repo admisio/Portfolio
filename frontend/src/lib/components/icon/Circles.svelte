eba41a9b4e642b24ae922d51218b5cff2a61b8f3
EETagent <vojta.jungmann@gmail.com>
2022-11-22 20:06:47 +0100

    refactor: move circles into seperate component

---
+++ frontend/src/lib/components/icon/Circles.svelte
@@ -0,0 +1,14 @@
+<div>
+	<span class="left-20 top-10 w-20 h-20 rounded-full bg-black bg-[#f8fbfc] opacity-100" />
+	<span class="left-14 -top-6 w-24 h-24 rounded-full bg-black bg-[#558cbd] opacity-50" />
+	<span class="w-28 h-28 bg-black bg-[#55b1bd] opacity-60" />
+</div>
+
+<style>
+	div {
+		@apply relative;
+	}
+	span {
+		@apply shadow-md absolute rounded-full p-1;
+	}
+</style>
