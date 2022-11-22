b3850c1928205ae4b44344aa057f877fe5002f9f
EETagent <vojta.jungmann@gmail.com>
2022-11-22 20:43:22 +0100

    feat: refactor dashboard, support for mobile screens, animated dash border

---
+++ frontend/src/lib/components/dashboard/DashboardInfoCard.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import Circles from '../icon/Circles.svelte';
+
+	export let title: string;
+</script>
+
+<div class="card infoCard relative">
+	<span class="absolute -left-8 -top-8">
+		<Circles />
+	</span>
+	<div class="mt-20 flex flex-col">
+		<h3>{title}</h3>
+		<slot />
+	</div>
+</div>
+
+<style>
+	.card {
+		@apply h-full;
+		@apply flex flex-col justify-between;
+
+		@apply bg-[#f8fbfc];
+		@apply rounded-3xl;
+		@apply px-7 py-10;
+	}
+	.card h3 {
+		@apply text-sspsBlue text-4xl md:text-2xl xl:text-4xl font-semibold;
+	}
+</style>
