b2cd902e987436b81318210fcb725f5864dc762a
Sebastian Pravda <sebastian.pravda@gmail.com>
2022-11-23 11:46:28 +0100

    feat: cli csv export

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,12 +1,11 @@
 use std::path::PathBuf;
+
+use clap::{arg, ArgAction, command, Command, value_parser};
+use sea_orm::{Database, DatabaseConnection};
 use url::Url;
 
-use clap::{arg, command, value_parser, ArgAction, Command};
-use sea_orm::{Database, DatabaseConnection};
-
-use ::entity::candidate::Entity as Candidate;
-use ::entity::parent::Entity as Parent;
-use sea_orm::*;
+use portfolio_core::{crypto, Query};
+use portfolio_core::candidate_details::{ApplicationDetails, EncryptedApplicationDetails};
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
@@ -15,8 +14,15 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .subcommand_required(true)
         .arg_required_else_help(true)
         .subcommand(
-            Command::new("portfolio")
-                .about("Database & Portfolio operations")
+            Command::new("export")
+                .about("Export all candidate data to a CSV file")
+                .arg(
+                    arg!(
+                        -o --output <PATH> "Output file path"
+                    )
+                    .required(true)
+                    .value_parser(value_parser!(PathBuf)),
+                )
                 .arg(
                     arg!(
                         -d --database <URL> "URL to the database or sql file with postgres:// or sqlite://"
@@ -25,6 +31,28 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
                     .required(true)
                     .value_parser(value_parser!(Url)),
                 )
+                .arg(
+                    arg!(
+                        -k --key <KEY> "AGE private key for decryption"
+                    )
+                    .required(false),
+                )
+                .arg(
+                    arg!(
+                        -p --password <PASSWORD> "Password for decryption"
+                    )
+                    .required(false),
+                )
+                .arg(
+                    arg!(
+                        -a --admin_id <ADMIN_ID> "Admin ID"
+                    )
+                    .required(false),
+                )
+        )
+        .subcommand(
+            Command::new("portfolio")
+                .about("Database & Portfolio operations")
                 .arg(
                     arg!(
                         -p --portfolio <PATH>  "Path to the portfolio root"
@@ -98,29 +126,52 @@ async fn main() -> Result<(), Box<dyn std::error::Error>> {
         .get_matches();
 
     match clap.subcommand() {
-        Some(("portfolio", sub_matches)) => {
-            let sqlite_url = sub_matches.get_one::<Url>("database").unwrap();
-
-            println!("Connecting to {:?}", sqlite_url);
-
-            if sqlite_url.scheme() != "sqlite" && sqlite_url.scheme() != "postgres" {
+        Some(("export", sub_matches)) => {
+            let db_url = sub_matches.get_one::<Url>("database").unwrap();
+            if db_url.scheme() != "sqlite" && db_url.scheme() != "postgres" {
                 return Err("URL scheme postgres:// or sqlite:// required")?;
             }
+            let db: DatabaseConnection = Database::connect(db_url.as_str()).await?;
 
-            let db: DatabaseConnection = Database::connect(sqlite_url.as_str()).await?;
+            let key = match (sub_matches.get_one::<String>("key"), sub_matches.get_one::<String>("password")) {
+                (Some(key), _) => {
+                    key.to_string()
+                },
+                (_, Some(password)) => {
+                    let admin_id = sub_matches.get_one::<String>("id").unwrap().parse::<i32>().unwrap();
+                    let admin = Query::find_admin_by_id(&db, admin_id)
+                        .await
+                        .map_err(|e| format!("Admin {} not found", admin_id))?
+                        .ok_or("Admin not found")?;
+                    crypto::decrypt_password(
+                        admin.private_key,
+                        password.to_string()
+                    ).await?
 
-            let entries = Candidate::find()
-                .join_rev(
-                    JoinType::InnerJoin,
-                    Parent::belongs_to(Candidate)
-                        .from(::entity::parent::Column::Application)
-                        .to(::entity::candidate::Column::Application)
-                        .into(),
-                )
-                .all(&db)
-                .await?;
+                },
+                _ => {
+                    unreachable!("Either key or password must be provided");
+                }
+            };
+            
+            let output = sub_matches.get_one::<PathBuf>("output").unwrap();
+            let mut csv = csv::Writer::from_path(output)?;
 
-            println!("Found {} entries", entries.len());
+            let candidates_with_parents = Query::list_all_candidates_with_parents(&db).await?;
+            for candidate in candidates_with_parents {
+                let application = candidate.application;
+
+                if let Ok(enc_details) = EncryptedApplicationDetails::try_from(candidate) {
+                    let details = enc_details.decrypt(key.to_string()).await?;
+                    csv.serialize(details)?;
+                } else {
+                    println!("Failed to decrypt candidate {} (Candidate data not set)", application);
+                }
+            }
+            csv.flush()?;
+        },
+        Some(("portfolio", sub_matches)) => {
+            todo!()
         }
         Some(("hash", sub_matches)) => {
             let input = sub_matches.get_one::<String>("input").unwrap();
--- core/src/database/query/candidate.rs
+++ core/src/database/query/candidate.rs
@@ -1,9 +1,9 @@
-use crate::{Query};
-
-use ::entity::{candidate, candidate::Entity as Candidate, parent};
 use sea_orm::*;
 use serde::Serialize;
 
+use ::entity::{candidate, candidate::Entity as Candidate, parent};
+
+use crate::Query;
 
 pub const PAGE_SIZE: u64 = 20;
 
@@ -17,6 +17,28 @@ pub struct CandidateParentResult {
 
     pub parent_name: Option<String>,
     pub parent_surname: Option<String>,
+}
+
+#[derive(FromQueryResult, Serialize)]
+pub struct CandidateWithParent { // TODO: use this instead of (Candidate, Parent)???
+    pub application: i32,
+    pub name: Option<String>,
+    pub surname: Option<String>,
+    pub birth_surname: Option<String>,
+    pub birthplace: Option<String>,
+    pub birthdate: Option<String>,
+    pub address: Option<String>,
+    pub telephone: Option<String>,
+    pub citizenship: Option<String>,
+    pub email: Option<String>,
+    pub sex: Option<String>,
+    pub study: Option<String>,
+    pub personal_identification_number: Option<String>,
+
+    pub parent_name: Option<String>,
+    pub parent_surname: Option<String>,
+    pub parent_telephone: Option<String>,
+    pub parent_email: Option<String>,
 }
 
 impl Query {
@@ -53,13 +75,30 @@ impl Query {
             query.fetch().await
         }
     }
+
+
+    pub async fn list_all_candidates_with_parents(
+        db: &DbConn,
+    ) -> Result<Vec<CandidateWithParent>, DbErr> {
+        Candidate::find()
+            .order_by(candidate::Column::Application, Order::Asc)
+            .join(JoinType::InnerJoin, candidate::Relation::Parent.def())
+            .column_as(parent::Column::Name, "parent_name")
+            .column_as(parent::Column::Surname, "parent_surname")
+            .column_as(parent::Column::Telephone, "parent_telephone")
+            .column_as(parent::Column::Email, "parent_email")
+            .into_model::<CandidateWithParent>()
+            .all(db)
+            .await
+    }
     
 }
 
 #[cfg(test)]
 mod tests {
-    use entity::candidate;
     use sea_orm::{ActiveModelTrait, Set};
+
+    use entity::candidate;
 
     use crate::Query;
     use crate::util::get_memory_sqlite_connection;
