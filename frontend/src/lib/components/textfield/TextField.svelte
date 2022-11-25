2fad0a4203c976ba2492793c9869fbb98de34a25
Sebastian Pravda <sebastian.pravda@gmail.com>
2022-11-25 18:46:04 +0100

    Squashed commit of the following:
    commit ec8ff2c03bd94b1bf5a8055a385bccb14ba48e76
    Author: Sebastian Pravda <sebastian.pravda@gmail.com>
    Date:   Fri Nov 25 18:44:16 2022 +0100
    
        fix: remove some ui elements
    
    commit 091c4fa63e1e2c374e449624dd5a384e4ad48532
    Author: Sebastian Pravda <sebastian.pravda@gmail.com>
    Date:   Fri Nov 25 18:38:50 2022 +0100
    
        feat: use yup for input validation
    
    commit abd0ba7ddcc938a680d71cc7a41d89f74915b5b9
    Author: Sebastian Pravda <sebastian.pravda@gmail.com>
    Date:   Fri Nov 25 18:23:48 2022 +0100
    
        fix: delete input.ts
    
    commit c305b330e5211b1d0a68f0d975e36b930a4ddb52
    Author: Sebastian Pravda <sebastian.pravda@gmail.com>
    Date:   Fri Nov 25 18:12:16 2022 +0100
    
        feat: api integration
    
    commit e1fd8c0fc4327020abcb0d8979d2353a8cd1f2af
    Author: Sebastian Pravda <sebastian.pravda@gmail.com>
    Date:   Fri Nov 25 18:02:27 2022 +0100
    
        feat: dot navigation
    
    commit 90bd277474493ed7d3e3cea2a7bd359f13374334
    Author: Sebastian Pravda <sebastian.pravda@gmail.com>
    Date:   Fri Nov 25 17:38:31 2022 +0100
    
        feat: input formatting
    
    commit 32a7901d6bf059b0b7818a81c577f5508af262ce
    Author: Sebastian Pravda <sebastian.pravda@gmail.com>
    Date:   Fri Nov 25 17:35:04 2022 +0100
    
        fix: remove unused validate method
    
    commit 7013a21be86ca8ffb04339a84c567937cf773895
    Author: Sebastian Pravda <sebastian.pravda@gmail.com>
    Date:   Fri Nov 25 17:31:16 2022 +0100
    
        refactor: rename format variable
    
    commit e86c997ea73ea67406e87687efa660f1bce9a0ff
    Author: Sebastian Pravda <sebastian.pravda@gmail.com>
    Date:   Fri Nov 25 15:10:11 2022 +0100
    
        feat: fill candidate details
    
    commit af28099a6a905d2729aaba12b628e5416d06c3d6
    Author: Sebastian Pravda <sebastian.pravda@gmail.com>
    Date:   Fri Nov 25 17:15:45 2022 +0100
    
        feat: use yup for validation

--- frontend/src/lib/components/textfield/TextField.svelte
+++ frontend/src/lib/components/textfield/TextField.svelte
@@ -1,10 +1,26 @@
 <script lang="ts">
 	export let type: 'text' | 'number' | 'tel' | 'e-mail' | "password" = 'text';
+	export let format: 'default' | 'email' | 'tel' | 'name' | 'number' | 'birthdate' | 'personalIdNumber' = 'default';
 	const typeAction = (node: HTMLInputElement) => {
 		node.type = type;
 	};
 	export let placeholder: string = '';
 	export let value: string = '';
+
+	$: {
+		if (format === 'tel') {
+			let x = value.replace(/\D/g, '').match(/(\d{0,3})(\d{0,3})(\d{0,3})(\d{0,3})/)!;
+			value = '+' + x[1]  + (x[2] ? ' ' + x[2] : '') + (x[3] ? ' ' + x[3] : '') + (x[4] ? ' ' + x[4] : '');
+		} else if (format === 'number') {
+			value = value.replace(/[^0-9]/g, '');
+		} else if (format === 'birthdate') { // TODO: more intuitive date input
+			let x = value.replace(/\D/g, '').match(/(\d{0,2})(\d{0,2})(\d{0,4})/)!;
+			value = x[1] + (x[2] ? '.' + x[2] : '') + (x[3] ? '.' + x[3] : '');
+		} else if (format === 'personalIdNumber') {
+			let x = value.replace(/\D/g, '').match(/(\d{0,6})(\d{0,4})/)!;
+			value = x[1] + (x[2] ? '/' + x[2] : '');
+		}
+	}
 
 	export let icon: boolean = false;
 	export let error: string = "";
