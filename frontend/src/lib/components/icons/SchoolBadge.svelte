47d90a0072091e87967a238ba3c51cc520d60a0b
EETagent <vojta.jungmann@gmail.com>
2022-11-29 11:42:35 +0100

    feat: move badge to own component

---
+++ frontend/src/lib/components/icons/SchoolBadge.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+    import lion from '$lib/assets/logo/lion.png';
+    </script>
+<div
+	class="py-3 px-6 md:py-4 md:px-8 rounded-[999px] shadow-2xl flex items-center justify-center transition-all duration-700 hover:shadow-md"
+>
+	<img class="object-cover" src={lion} alt="" />
+</div>
+
+<style>
+
+</style>
