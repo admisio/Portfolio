31185813d120999f77e67dc5050881d6c6d80d8e
EETagent <vojta.jungmann@gmail.com>
2022-11-29 11:24:40 +0100

    feat: refactor form components

---
+++ frontend/src/lib/components/textfield/NumberField.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import TextField from './TextField.svelte';
+
+	export let placeholder: string = '';
+	export let value: string = '';
+	export let error: string = '';
+
+	// Number formatting
+	$: {
+		value = value.replace(/[^0-9]/g, '');
+	}
+</script>
+
+<TextField bind:error bind:value on:keydown on:keyup on:change type="number" {placeholder} icon />
