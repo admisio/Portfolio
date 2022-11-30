bfa04e02064ea7dad1f9a73b0532561d20d68867
Sebastian Pravda <sebastian.pravda@gmail.com>
2022-11-30 15:47:41 +0100

    feat: alert functionality PoC

---
+++ frontend/src/stores/portfolio.ts
@@ -0,0 +1,18 @@
+import { apiFetchSubmissionProgress } from "../api/candidate";
+import { writable } from "svelte/store";
+
+export interface SubmissionProgress {
+    status?: number;
+    files?: [number];
+}
+
+export const submissionProgress = writable<SubmissionProgress>();
+
+export async function fetchSubmProgress() {
+    try {
+        let prog = await apiFetchSubmissionProgress();
+        submissionProgress.set(prog);
+    } catch (e) {
+        console.error(e);
+    }
+}
