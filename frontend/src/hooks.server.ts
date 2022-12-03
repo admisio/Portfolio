dfd746be5acd3ca124da0c1ec644d3295a5ce535
EETagent <vojta.jungmann@gmail.com>
2022-12-03 16:18:36 +0100

    feat: ssr fetch hook

---
+++ frontend/src/hooks.server.ts
@@ -0,0 +1,15 @@
+import type {  HandleFetch } from '@sveltejs/kit';
+
+export const handleFetch: HandleFetch = async ({ request, fetch, event,}) => {
+	console.log(`SSR: handleFetch() BEFORE: ${request.method} ${request.url}`);
+
+    const cookie = event.request.headers.get('cookie') || '';
+
+    request.headers.set('cookie', cookie);
+
+	request.headers.append('Origin', event.url.origin);
+
+    console.log(`SSR: handleFetch() AFTER:  ${request.method} ${request.url}`);
+
+	return fetch(request);
+};
