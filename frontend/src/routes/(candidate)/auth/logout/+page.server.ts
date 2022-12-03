93041d16b6ea300c8e81301ea6d7a3f892f59602
EETagent <vojta.jungmann@gmail.com>
2022-12-03 16:18:50 +0100

    feat: ssr fetch logout

--- frontend/src/routes/(candidate)/auth/logout/+page.server.ts
+++ frontend/src/routes/(candidate)/auth/logout/+page.server.ts
@@ -1,13 +1,13 @@
 import type { PageServerLoad } from './$types';
 
 import { redirect } from '@sveltejs/kit';
-import { logout } from '$lib/stores/candidate';
+import { apiLogout } from '$lib/@api/candidate';
 
-export const load: PageServerLoad = async ({ cookies }) => {
-	// TODO: Nefunguje?!
-	await logout();
-    console.log(cookies);
-	cookies.delete('id', {path: '/'});
-	cookies.delete('key', {path: '/'});
+export const load: PageServerLoad = async ({ fetch, cookies }) => {
+	await apiLogout(fetch);
+
+    cookies.delete('id', { path: '/' });
+    cookies.delete('key', { path: '/' });
+
 	throw redirect(302, '/auth/login');
 };
