6ece58aef4782e277eec9757910d3f8e12b332ec
EETagent <vojta.jungmann@gmail.com>
2022-12-03 22:21:36 +0100

    refactor: remove stores no longer needed after SSR

--- frontend/src/lib/stores/candidate.ts
+++ frontend/src/lib/stores/candidate.ts
@@ -1,6 +1,4 @@
-import { apiFetchDetails, apiFillDetails, apiLogin, apiLogout } from "../@api/candidate";
 import { writable } from "svelte/store";
-import { browser } from "$app/environment";
 
 export interface CandidateData {
     name?: string;
@@ -26,66 +24,3 @@ export interface CandidateLogin {
 }
 
 export const candidateData = writable<CandidateData>({});
-
-
-if (browser) {
-    const name = localStorage.getItem("name");
-    const surname = localStorage.getItem("surname");
-    const email = localStorage.getItem("email");
-    if (name && email && surname) {
-        candidateData.set({
-            name,
-            surname,
-            email
-        });
-    }
-}
-candidateData.subscribe((val) => {
-    if (browser) {
-        localStorage.setItem("name", val.name ?? "");
-        localStorage.setItem("surname", val.surname ?? "");
-        localStorage.setItem("email", val.email ?? "");
-    }
-})
-
-
-export async function login(data: CandidateLogin) {
-    // TODO: handle errors
-    try {
-        const applicationId = await apiLogin(data); // TODO: set candidate data from response to store
-        console.log("login result: " + applicationId);
-    } catch (e) {
-        console.error("login failed");
-        throw e;
-    }
-}
-
-export async function logout() {
-    // TOOD: handle errors
-    try {
-        await apiLogout();
-        candidateData.set({});
-    } catch (e) {
-        console.error(e);
-    }
-}
-
-export async function fillDetails(data: CandidateData) {
-    try {
-        const res = await apiFillDetails(data);
-        candidateData.set(res);
-    } catch (e) {
-        console.error(e);
-        throw e;
-    }
-}
-
-export async function fetchDetails() {
-    try {
-        const res = await apiFetchDetails();
-        candidateData.set(res);
-    } catch (e) {
-        console.error(e);
-        throw e;
-    }
-}
