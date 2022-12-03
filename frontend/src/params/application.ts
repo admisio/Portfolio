8a0027ce396597deef65812052a022461f9da5c1
EETagent <vojta.jungmann@gmail.com>
2022-12-04 00:59:50 +0100

    feat: add real param validation for applicationId

--- frontend/src/params/application.ts
+++ frontend/src/params/application.ts
@@ -1,6 +1,8 @@
 import type { ParamMatcher } from '@sveltejs/kit';
 
 export const match: ParamMatcher = (param) => {
-	//TODO: Application regex
-	return /^\d+$/.test(param);
+	const isNumber = /^\d{6}(?:\d{1})?$/.test(param);
+	const isValid = param.startsWith('101') || param.startsWith('102') || param.startsWith('103');
+	
+	return isNumber && isValid;
 };
