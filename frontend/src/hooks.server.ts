58d5fb6dbc079b6c3fc755b9074391420787f589
EETagent <vojta.jungmann@gmail.com>
2022-12-03 18:17:00 +0100

    fix: ssr on localhost

--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -1,3 +1,4 @@
+import { API_URL } from '$lib/@api';
 import type {  HandleFetch } from '@sveltejs/kit';
 
 export const handleFetch: HandleFetch = async ({ request, fetch, event,}) => {
@@ -6,10 +7,13 @@ export const handleFetch: HandleFetch = async ({ request, fetch, event,}) => {
     const cookie = event.request.headers.get('cookie') || '';
 
     console.log(`SSR: handleFetch() cookie: ${cookie}`);
-
+    
     request.headers.set('cookie', cookie);
 
-	request.headers.append('Origin', event.url.origin);
+    request = new Request(
+        request.url.replace(API_URL, 'http://127.0.0.1:8000'),
+        request
+     );
 
     console.log(`SSR: handleFetch() AFTER:  ${request.method} ${request.url}`);
 
