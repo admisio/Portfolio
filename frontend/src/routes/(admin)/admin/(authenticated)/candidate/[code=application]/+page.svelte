b77e62e30cc8c29e95555f945686a6235561af3a
EETagent <vojta.jungmann@gmail.com>
2022-12-05 14:41:35 +0100

    refactor:  candidate details location

--- frontend/src/routes/(admin)/admin/(authenticated)/candidate/[code=application]/+page.svelte
+++ frontend/src/routes/(admin)/admin/(authenticated)/candidate/[code=application]/+page.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import CandidateDetails from '$lib/components/list/CandidateDetails.svelte';
+	import CandidateDetails from '$lib/components/admin/list/CandidateDetails.svelte';
 	import type { PageData } from './$types';
 
 	export let data: PageData;
