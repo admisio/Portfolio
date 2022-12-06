047476af2a3dcb2f5b8bfd2b112dc858fdd44b2b
EETagent <vojta.jungmann@gmail.com>
2022-12-06 16:01:23 +0100

    feat: nested form

--- frontend/src/lib/stores/candidate.ts
+++ frontend/src/lib/stores/candidate.ts
@@ -1,21 +1,25 @@
 import { writable } from 'svelte/store';
 
 export interface CandidateData {
-	name?: string;
-	surname?: string;
-	birthplace?: string;
-	birthdate?: string;
-	address?: string;
-	telephone?: string;
-	citizenship?: string;
-	email?: string;
-	sex?: string;
-	study?: string;
-	personalIdNumber?: string;
-	parentName?: string;
-	parentSurname?: string;
-	parentTelephone?: string;
-	parentEmail?: string;
+	candidate: {
+		name?: string;
+		surname?: string;
+		birthplace?: string;
+		birthdate?: string;
+		address?: string;
+		telephone?: string;
+		citizenship?: string;
+		email?: string;
+		sex?: string;
+		study?: string;
+		personalIdNumber?: string;
+	};
+	parents: Array<{
+		name?: string;
+		surname?: string;
+		telephone?: string;
+		email?: string;
+	}>;
 }
 
 export interface CandidatePreview {
