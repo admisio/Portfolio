6b94440e267258ba25b087f864974feed502b5b0
EETagent <vojta.jungmann@gmail.com>
2022-12-14 21:09:13 +0100

    fix: vite proxy

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -6,7 +6,7 @@ const config: UserConfig = {
 	server: {
 		proxy: {
 			'/api': {
-				target: 'http://localhost:8000',
+				target: 'http://127.0.0.1:8000',
 				changeOrigin: true,
 				rewrite: (path) => path.replace(/^\/api/, '')
 			}
