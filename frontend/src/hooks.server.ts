fc09a064ba13644a613df927b84d0deb389c6b5b
EETagent <vojta.jungmann@gmail.com>
2022-12-14 18:05:41 +0100

    feat: dotenv for API

--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -1,7 +1,10 @@
 import { API_URL } from '$lib/@api';
 import type { HandleFetch } from '@sveltejs/kit';
+import * as dotenv from 'dotenv'
 
 export const handleFetch: HandleFetch = async ({ request, fetch, event }) => {
+	dotenv.config();
+
 	console.log(`SSR: handleFetch() BEFORE: ${request.method} ${request.url}`);
 
 	const cookie = event.request.headers.get('cookie') || '';
@@ -10,7 +13,7 @@ export const handleFetch: HandleFetch = async ({ request, fetch, event }) => {
 
 	request.headers.set('cookie', cookie);
 
-	request = new Request(request.url.replace(API_URL, 'http://127.0.0.1:8000'), request);
+	request = new Request(request.url.replace(API_URL, process.env.PORTFOLIO_API_URL ?? 'http://127.0.0.1:8000'), request);
 
 	console.log(`SSR: handleFetch() AFTER:  ${request.method} ${request.url}`);
 
