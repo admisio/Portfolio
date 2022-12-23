7fed62ff75c26a65d5de01e90be073f8dbbffdbd
Sebastian Pravda <sebastian.pravda@gmail.com>
2022-12-23 17:43:36 +0100

    fix: remove onClick

--- frontend/src/lib/components/dashboard/InfoButton.svelte
+++ frontend/src/lib/components/dashboard/InfoButton.svelte
@@ -37,7 +37,7 @@
 			<svg class="icon" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 16v1a3 3 0 003 3h10a3 3 0 003-3v-1m-4-4l-4 4m0 0l-4-4m4 4V4"></path></svg>
 			</span>
 		{:else}
-		<span on:click={(_) => download()} on:keydown={(_) => download()} use:tippy={{
+		<span use:tippy={{
 			content: "Nelze stáhnout, portfolio nebylo odevzdáno",
 			placement: 'top',
 			showOnCreate: false,
