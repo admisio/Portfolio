87c35107ce01bfd380697ad8b93b10cbda359416
EETagent <vojta.jungmann@gmail.com>
2023-01-12 13:19:17 +0100

    feat: add auto logout

--- frontend/src/routes/(admin)/admin/(authenticated)/+layout.server.ts
+++ frontend/src/routes/(admin)/admin/(authenticated)/+layout.server.ts
@@ -1,10 +1,19 @@
 import type { LayoutServerLoad } from './$types';
 
 import { redirect } from '@sveltejs/kit';
+import { apiWhoami } from '$lib/@api/admin';
 
-export const load: LayoutServerLoad = ({ cookies }) => {
+export const load: LayoutServerLoad = async ({ cookies, fetch }) => {
 	const isAuthenticated = cookies.get('id');
-	if (!isAuthenticated) {
-		throw redirect(302, '/admin/auth/login');
+
+	if (isAuthenticated) {
+		const whoami = await apiWhoami(fetch).catch((e) => {
+			throw redirect(302, '/admin/auth/logout');
+		});
+		return {
+			whoami: whoami
+		};
+	} else {
+		throw redirect(302, '/admin/auth/logout');
 	}
 };
