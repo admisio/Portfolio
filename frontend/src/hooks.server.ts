a5c238640ab1ce3a068e509dd69c3ad73b35109b
Sebastian Pravda <sebastian.pravda@gmail.com>
2023-01-12 18:59:25 +0100

    fix: logging

--- frontend/src/hooks.server.ts
+++ frontend/src/hooks.server.ts
@@ -3,12 +3,7 @@ import * as dotenv from 'dotenv';
 
 export const handleFetch: HandleFetch = async ({ request, fetch, event }) => {
 	dotenv.config();
-
-	console.log(`SSR: handleFetch() BEFORE: ${request.method} ${request.url}`);
-
 	const cookie = event.request.headers.get('cookie') || '';
-
-	console.log(`SSR: handleFetch() cookie: ${cookie}`);
 
 	request.headers.set('cookie', cookie);
 
@@ -21,8 +16,6 @@ export const handleFetch: HandleFetch = async ({ request, fetch, event }) => {
 	url.pathname = url.pathname.replace(/^\/api/, '');
 
 	request = new Request(url, request);
-
-	console.log(`SSR: handleFetch() AFTER:  ${request.method} ${request.url}`);
 
 	return fetch(request);
 };
