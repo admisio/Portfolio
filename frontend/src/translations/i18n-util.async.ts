474caab0e1e3df4a199ad539625940ff8c358e38
EETagent <vojta.jungmann@gmail.com>
2023-01-20 13:19:11 +0100

    feat: deportace Němců

--- frontend/src/translations/i18n-util.async.ts
+++ frontend/src/translations/i18n-util.async.ts
@@ -6,6 +6,7 @@ import type { Locales, Translations } from './i18n-types.js'
 import { loadedFormatters, loadedLocales, locales } from './i18n-util.js'
 
 const localeTranslationLoaders = {
+	cs: () => import('./cs/index.js'),
 }
 
 const updateDictionary = (locale: Locales, dictionary: Partial<Translations>): Translations =>
