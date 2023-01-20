cdb27d79da9ad476c68eb8e6b0316b4940950de9
EETagent <vojta.jungmann@gmail.com>
2023-01-20 14:14:19 +0100

    feat? initial translation

--- frontend/src/translations/cs/index.ts
+++ frontend/src/translations/cs/index.ts
@@ -1,6 +1,15 @@
-import type { BaseTranslation } from '../i18n-types.js'
+import type { BaseTranslation } from '../i18n-types.js';
 
 const cs: BaseTranslation = {
-}
+	candidate: {
+		auth: {
+			login: {
+				title: 'Přihlášení',
+				description:
+					'Evidenční číslo je jedinečné číslo přidělené uchazeči, které slouží k jeho identifikaci\napřihlášení se do systému.'
+			}
+		}
+	}
+};
 
-export default cs
+export default cs;
