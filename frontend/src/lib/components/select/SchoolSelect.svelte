6b07e2c629cb43ac51879c0ebf2a7a0a2352be18
Sebastian Pravda <sebastian.pravda@gmail.com>
2023-01-21 23:26:32 +0100

    feat: SchoolSelect component

---
+++ frontend/src/lib/components/select/SchoolSelect.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+import AutoComplete from 'simple-svelte-autocomplete';
+import { onMount } from 'svelte';
+// import schoollistString from '$lib/assets/schoollist.txt';
+
+let schools: string[] = [];
+
+onMount(async () => {
+   schools = await fetch('/schoollist.txt')
+    .then((response) => response.text())
+    .then((text) => text.split(';'));
+});
+
+export let selectedSchool: string = '';
+</script>
+
+Selected color: {selectedSchool}
+<AutoComplete items={schools} bind:selectedItem={selectedSchool} />
