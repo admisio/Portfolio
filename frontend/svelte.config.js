bcd62e3cd52bb41f418823bf3f18ad7b7272ef01
EETagent <vojta.jungmann@gmail.com>
2023-02-13 18:41:56 +0100

    feat: use vitePreprocess

--- frontend/svelte.config.js
+++ frontend/svelte.config.js
@@ -1,10 +1,10 @@
 import adapter from '@sveltejs/adapter-node';
-import preprocess from 'svelte-preprocess';
+import { vitePreprocess } from '@sveltejs/kit/vite';
 import path from 'path';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: [preprocess()],
+	preprocess: [vitePreprocess()],
 	kit: {
 		adapter: adapter({ out: 'build' }),
 		alias: {
