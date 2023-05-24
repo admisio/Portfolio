553753a5026deecd0349e9e17210acffacc29ed5
Vojtěch Jungmann <vojta.jungmann@gmail.com>
2023-05-24 19:55:27 +0200

    fix: formatting

--- frontend/windi.config.ts
+++ frontend/windi.config.ts
@@ -3,7 +3,15 @@ import { defineConfig } from 'windicss/helpers';
 export default defineConfig({
 	extract: {
 		include: ['src/**/*.{svelte,html,js,ts}'],
-		exclude: ['node_modules', '.git', 'dist', 'build', 'public', "src/translations", "src/lib/assets"],
+		exclude: [
+			'node_modules',
+			'.git',
+			'dist',
+			'build',
+			'public',
+			'src/translations',
+			'src/lib/assets'
+		]
 	},
 	theme: {
 		extend: {
